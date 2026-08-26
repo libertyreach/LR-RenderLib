dcae12a1855e3991c0c79fcb799ce8ae313ede17
Henry Walters <henrywalters20@gmail.com>
2026-08-26 16:45:17 -0400

    Fix colors

---
+++ include/renderlib/colors.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "types.h"
+
+namespace renderlib {
+const vec4i WHITE{{255, 255, 255, 255}};
+const vec4i BLACK{{0, 0, 0, 255}};
+const vec4i RED{{255, 0, 0, 255}};
+const vec4i GREEN{{0, 255, 0, 255}};
+const vec4i BLUE{{0, 0, 255, 255}};
+}  // namespace renderlib
